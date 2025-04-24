0ae277a003d00e67c50975a4ed91a3b22e01df7d
Aneta Kasprzak <anet.kasprzak@gmail.com>
2025-04-24 15:00:08 +0100

    Using Signals & Resetting The Form After Submission

--- src/app/investment-result/investment-result.component.ts
+++ src/app/investment-result/investment-result.component.ts
@@ -1,5 +1,5 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component, Input, input } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-investment-result',
@@ -9,21 +9,21 @@ import { Component, Input, input } from '@angular/core';
   styleUrl: './investment-result.component.css'
 })
 export class InvestmentResultComponent {
-  // results = input<{
-  //   year: number,
-  //   interest: number,
-  //   valueEndOfYear: number,
-  //   annualInvestment: number,
-  //   totalInterest: number,
-  //   totalAmountInvested: number,
-  // }[]>()
-
-  @Input() results?: {
+  results = input<{
     year: number,
     interest: number,
     valueEndOfYear: number,
     annualInvestment: number,
     totalInterest: number,
     totalAmountInvested: number,
-  }[];
+  }[]>()
+
+  // @Input() results?: {
+  //   year: number,
+  //   interest: number,
+  //   valueEndOfYear: number,
+  //   annualInvestment: number,
+  //   totalInterest: number,
+  //   totalAmountInvested: number,
+  // }[];
 }
