d907a13dac4b5c620b4f490aa3a5f6af5acf6eb5
Aneta Kasprzak <anet.kasprzak@gmail.com>
2025-04-25 13:35:56 +0100

    doesnt work - wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
-import type { InvestmentInput } from './investment-input.model';
 import { InvestmentResultComponent } from './investment-result/investment-result.component';
 
 @Component({
@@ -11,45 +10,7 @@ import { InvestmentResultComponent } from './investment-result/investment-result
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  // resultsData?: {
-  //   year: number,
-  //   interest: number,
-  //   valueEndOfYear: number,
-  //   annualInvestment: number,
-  //   totalInterest: number,
-  //   totalAmountInvested: number,
-  // }[];
 
-  resultsData = signal<{
-    year: number,
-    interest: number,
-    valueEndOfYear: number,
-    annualInvestment: number,
-    totalInterest: number,
-    totalAmountInvested: number,
-  }[] | undefined>(undefined);
 
-  onCalculateInvestmentResults(data: InvestmentInput) {
-    const { initialInvestment, annualInvestment, duration, expectedReturn } = data;
-    const annualData = [];
-    let investmentValue = initialInvestment;
 
-    for (let i = 0; i < duration; i++) {
-      const year = i + 1;
-      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-      investmentValue += interestEarnedInYear + annualInvestment;
-      const totalInterest =
-        investmentValue - annualInvestment * year - initialInvestment;
-      annualData.push({
-        year: year,
-        interest: interestEarnedInYear,
-        valueEndOfYear: investmentValue,
-        annualInvestment: annualInvestment,
-        totalInterest: totalInterest,
-        totalAmountInvested: initialInvestment + annualInvestment * year,
-      });
-    }
-
-    this.resultsData.set(annualData)
-  }
 }
--- src/app/investment-result/investment-result.component.ts
+++ src/app/investment-result/investment-result.component.ts
@@ -1,5 +1,6 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component, input } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-result',
@@ -9,21 +10,9 @@ import { Component, input } from '@angular/core';
   styleUrl: './investment-result.component.css'
 })
 export class InvestmentResultComponent {
-  results = input<{
-    year: number,
-    interest: number,
-    valueEndOfYear: number,
-    annualInvestment: number,
-    totalInterest: number,
-    totalAmountInvested: number,
-  }[]>()
+  private investmentService = inject(InvestmentService);
 
-  // @Input() results?: {
-  //   year: number,
-  //   interest: number,
-  //   valueEndOfYear: number,
-  //   annualInvestment: number,
-  //   totalInterest: number,
-  //   totalAmountInvested: number,
-  // }[];
+  get results() {
+    return this.investmentService.resultData;
+  }
 }
